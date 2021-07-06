0b6f57948a5f3a80c1f7c43f9b20a7ce941abd10
caijinglong-b-imac <cjl_spy@163.com>
2021-07-06 08:47:56 +0800

    Update content

--- lib/src/dingtalk_bot_sender_base.dart
+++ lib/src/dingtalk_bot_sender_base.dart
@@ -5,7 +5,58 @@ import 'package:http/http.dart' as http;
 
 import 'at.dart';
 
-/// 主类
+/// 发送的主类, 主要目的就是可以使用钉钉的机器人发送消息到群里
+///
+/// 使用示例如下
+///
+/// 为简单, 就使用关键字字段, 如果有加签需求, 则传入 appsecret
+///
+/// ```dart
+///
+///   final sender = DingTalkSender(
+///     hookUrl: hookUrl,
+///     keyword: keyword,
+///     appsecret: appsecret,
+///   );
+///
+///   final at = At(isAtAll: true);
+///
+///   await sender.sendText('1', at: at);
+///
+///   final markdown = '''
+/// 标题
+/// # 一级标题
+/// ## 二级标题
+/// ### 三级标题
+/// #### 四级标题
+/// ##### 五级标题
+/// ###### 六级标题
+///
+/// 引用
+/// > A man who stands for nothing will fall for anything.
+///
+/// 文字加粗、斜体
+/// **bold**
+/// *italic*
+///
+/// 链接
+/// [this is a link](http://name.com)
+///
+/// 图片
+/// ![](http://name.com/pic.jpg)
+///
+/// 无序列表
+/// - item1
+/// - item2
+///
+/// 有序列表
+/// 1. item1
+/// 2. item2
+///   ''';
+///
+///   final at2 = At(userIds: ['manager5664']);
+///   await sender.sendMarkdown(markdown, at: at2);
+/// ```
 class DingTalkSender {
   /// webhook url
   final String hookUrl;
@@ -39,6 +90,7 @@ class DingTalkSender {
     return Uri.parse('$hookUrl${_sign()}');
   }
 
+  /// 发送 markdown 文本
   Future<void> sendMarkdown(String markdownText, {At? at}) async {
     final bodyMap = {
       'msgtype': 'markdown',
@@ -59,11 +111,20 @@ class DingTalkSender {
     );
   }
 
+  /// 发送纯文本
   Future<void> sendText(String text, {At? at}) async {
+    var content = text;
+
+    if (keyword != null) {
+      if (!text.contains(keyword!)) {
+        content = '$keyword $content';
+      }
+    }
+
     final bodyMap = {
       'msgtype': 'text',
       'text': {
-        'content': '$keyword $text',
+        'content': content,
       },
       'at': at?.toMap() ?? {},
     };
